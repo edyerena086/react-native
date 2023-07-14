f2376cdc3bd85526bbf1be8ff0541b6f98f81daa
edyerena086 <edyerena086@gmail.com>
2023-07-14 12:45:27 -0600

    Added icons into the project and bottom tab nav

--- src/navigation/bottom.tab.navigation.tsx
+++ src/navigation/bottom.tab.navigation.tsx
@@ -1,6 +1,7 @@
 // Import core libraries
 import React, {FC} from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import FontAwemoseIcon from 'react-native-vector-icons/FontAwesome'
 
 // Import screens
 import {SimpleFormScreen, CameraScreen, FlatListScreen} from '../screens'
@@ -9,7 +10,26 @@ const Tab = createBottomTabNavigator()
 
 const BottomTabNavigation: FC = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName
+
+          if (route.name === 'simpleForm') {
+            iconName = focused ? 'file-text' : 'file-text-o'
+          } else if (route.name === 'camera') {
+            iconName = focused ? 'camera' : 'camera'
+          } else if (route.name === 'flatList') {
+            iconName = focused ? 'list' : 'list'
+          }
+
+          // You can return any component that you like here!
+          return <FontAwemoseIcon name={iconName} size={size} color={color} />
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
       <Tab.Screen name={'simpleForm'} component={SimpleFormScreen} />
       <Tab.Screen name={'camera'} component={CameraScreen} />
       <Tab.Screen name={'flatList'} component={FlatListScreen} />

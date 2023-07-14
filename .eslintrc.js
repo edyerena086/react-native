d4dd84469c62ea6ce9e23ada8a4b5a4b6eaefcd9
edyerena086 <edyerena086@gmail.com>
2023-07-14 10:07:22 -0600

    Added eslint rule and modified run ios script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-};
+  rules: {
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+      },
+    ],
+  },
+}
